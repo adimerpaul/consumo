6c14779e9e21577f7deda18206be6aad19eaf9c1
adimerpaul <adimer101@gmail.com>
2021-08-30 08:06:25 -0400

    ultimas modificaciones

---
+++ consumoback/database/migrations/2021_08_20_134305_create_licencias_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLicenciasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('licencias', function (Blueprint $table) {
+            $table->id();
+            $table->date("fecha");
+            $table->time("hora");
+            $table->date("fechalimite");
+            $table->string("tipo");
+            $table->unsignedBigInteger("user_id");
+            $table->foreign("user_id")->references('id')->on('users');
+            $table->unsignedBigInteger("contribuyente_id");
+            $table->foreign("contribuyente_id")->references('id')->on('contribuyentes');
+            $table->unsignedBigInteger("caso_id");
+            $table->foreign("caso_id")->references('id')->on('casos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('licencias');
+    }
+}
