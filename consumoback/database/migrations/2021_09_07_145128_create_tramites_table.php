be1d8f7d4be8532571729a78100ef7baa7459f91
adimerpaul <adimer101@gmail.com>
2021-09-08 08:31:31 -0400

    modelado base de datos

---
+++ consumoback/database/migrations/2021_09_07_145128_create_tramites_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTramitesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tramites', function (Blueprint $table) {
+            $table->id();
+            $table->string("nrotramite");
+            $table->date("fecha");
+            $table->time("hora");
+            $table->dateTime("tramitador");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tramites');
+    }
+}
--- consumoback/database/seeders/CasoSeeder.php
+++ consumoback/database/seeders/CasoSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CasoSeeder extends Seeder
 {
@@ -13,6 +14,11 @@ class CasoSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('casos')->insert([
+           ["clasificacion"=>'DISCOTECA','caso'=>'DE MARTES  A SABADO','inicio'=>"20:00",'fin'=>'03:00','tipo'=>'A'],
+            ["clasificacion"=>'KARAOKE','caso'=>'DE MARTES  A SABADO','inicio'=>"20:00",'fin'=>'03:00','tipo'=>'A'],
+            ["clasificacion"=>'ESTABLECIMIENTO DE COMERCIALIZACION DE BEBIDAS ALCOHOLICAS','caso'=>'TODOS LOS DIAS','inicio'=>"09:00",'fin'=>'10:00','tipo'=>'B'],
+            ["clasificacion"=>'PLANTA DE ELABORACION Y/O PRODUCCION DE BEBIDAS ALCOHOLICAS','caso'=>'DE ACUERDO A HORARIO LABORAL','inicio'=>"08:00",'fin'=>'18:00','tipo'=>'B'],
+        ]);
     }
 }
